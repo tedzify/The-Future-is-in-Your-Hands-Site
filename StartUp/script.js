3eccc416445aaf4dc4bf7d340780839151caca24
DwaynePat <115750252+DwaynePat@users.noreply.github.com>
2024-11-24 12:51:08 +0800

    Oki

---
+++ StartUp/script.js
@@ -0,0 +1,13 @@
+ /*function onClick(){
+   window.location = "";
+    document.write("You will be redirected to the main page in 10 seconds");
+    setTimeout('Redirect()', 3000);
+    document.getElementById('rectangle-button').addEventListener('click', () => {
+        alert('Button clicked!');
+})
+}*/
+// Add click event listener to the button
+document.getElementById('rectangle-button').addEventListener('click', () => {
+    alert('Button clicked!');
+  });
+  
