a7b0897d7064022edbc2db94f7732f3d113e5c18
tedzify <vontedzify@gmail.com>
2024-12-04 11:23:03 +0800

    File path changes and optimizations

--- src/js/StartUp.js
+++ src/js/StartUp.js
@@ -1,5 +1,12 @@
-// JavaScript to handle button redirection
-document.getElementById("rectangle-button").addEventListener("click", function () {
-  // Redirect to the homepage inside the specified folder
-  window.location.href = "./Homepage/Homepage.html"; // Replace "folder-name" with the correct folder name
+/*function onClick(){
+   window.location = "";
+    document.write("You will be redirected to the main page in 10 seconds");
+    setTimeout('Redirect()', 3000);
+    document.getElementById('rectangle-button').addEventListener('click', () => {
+        alert('Button clicked!');
+})
+}*/
+// Add click event listener to the button
+document.getElementById("rectangle-button").addEventListener("click", () => {
+  alert("Button clicked!");
 });
